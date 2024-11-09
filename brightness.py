99d2166bc03bfa38fa87a9ee99832f34852f9cd1
gandres42 <gandres2416@gmail.com>
2024-11-08 23:02:20 -0800

    use homography to center grid output on robot, should be better suited for relative localization approach

--- brightness.py
+++ brightness.py
@@ -2,13 +2,45 @@ import cv2
 import numpy as np
 import pyrealsense as pyrs
 from pyrealsense.constants import rs_option # type: ignore
+import rclpy
+from rclpy.node import Node
+from nav_msgs.msg import OccupancyGrid, MapMetaData
 
-cv2.namedWindow("preview")
+class Grid(Node):
+    def __init__(self):
+        super().__init__('grid') # type: ignore
+        self.publisher_ = self.create_publisher(OccupancyGrid, '/light_grid', 10)
+
+    def publish(self,img):
+        msg = OccupancyGrid()
+        msg.header.frame_id = 'world'
+        msg.header.stamp = self.get_clock().now().to_msg()
+
+        # Fill in metadata (MapMetaData)
+        msg.info = MapMetaData()
+        msg.info.resolution = 0.01  # Each cell is 10cm x 10cm
+        msg.info.width = img.shape[0] # Width of the grid in cells
+        msg.info.height = img.shape[1] # Height of the grid in cells
+        msg.info.origin.position.x = 0.0
+        msg.info.origin.position.y = 0.0
+        msg.info.origin.position.z = 0.0
+
+        # Fill in data
+        print(len([-1] * (msg.info.width * msg.info.height)))
+        # print(img.flatten().as_list())
+        msg.data = img.flatten('F').tolist()
+        
+
+        self.publisher_.publish(msg)
+
 
 serv = pyrs.Service()
 custom_options = [(rs_option.RS_OPTION_COLOR_EXPOSURE, 156), (rs_option.RS_OPTION_COLOR_ENABLE_AUTO_EXPOSURE, 0)]
 cam = serv.Device(device_id = 0, streams = [pyrs.stream.ColorStream(fps = 60), ]) # type: ignore
 cam.set_device_options(*zip(*custom_options))
+
+rclpy.init()
+node = Grid()
 
 while True:
     cam.wait_for_frames()
@@ -16,20 +48,12 @@ while True:
     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
     noir_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(noir_frame, 60, 255, cv2.THRESH_BINARY)
-    # hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # #set the lower and upper bounds for the green hue
-    # lower_white = np.array([50,50,50])
-    # upper_white = np.array([255,255,255])
+    new_thresh = ((thresh / 255) * 127).astype(np.int8)
+    node.publish(new_thresh)
 
-    # #create a mask for green colour using inRange function
-    # mask = cv2.inRange(hsv, lower_white, upper_white)
-
-    #perform bitwise and on the original image arrays using the mask
-    # res = cv2.bitwise_and(frame, frame, mask=mask)
-    cv2.imshow("preview", thresh)
-    key = cv2.waitKey(20)
-    if key == 27: # exit on ESC
+    cv2.imshow('Camera', thresh)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-cv2.destroyWindow("preview")
+        
+rclpy.shutdown()
--- main.py
+++ main.py
@@ -6,79 +6,35 @@ from geometry_msgs.msg import PoseStamped
 from constants import aruco_positions, mtx, dist
 import rclpy
 import pyrealsense as pyrs
+from nav_msgs.msg import OccupancyGrid, MapMetaData
 
 INVERSE_LOCALIZATION = False
 DISPLAY = True
 
-class Transformer(Node):
-    def __init__(self, init_position, init_orientation):
-        super().__init__('overthruster') # type: ignore
-        self.init_position = init_position
-        self.init_orientation = init_orientation
-        self.subscription = self.create_subscription(
-            PoseStamped,
-            '/dlio/odom_node/pose',
-            self.listener_callback,
-            10
-        )
-        self.publisher_ = self.create_publisher(PoseStamped, '/adjusted_pose', 10)
-
-    def listener_callback(self, msg):
-        position = (np.array([
-            msg.pose.position.x,
-            msg.pose.position.y,
-            msg.pose.position.z
-        ]) @ self.init_orientation) + self.init_position # type: ignore
-        orientation = Rot.from_matrix(
-            Rot.from_quat([
-                msg.pose.orientation.x,
-                msg.pose.orientation.y,
-                msg.pose.orientation.z,
-                msg.pose.orientation.w
-            ]).as_matrix() @ self.init_orientation
-        ).as_quat() # type: ignore
-
-        new_msg = PoseStamped()
-
-        new_msg.header = msg.header
-        new_msg.header.stamp = self.get_clock().now().to_msg()
-
-        new_msg.pose.orientation.x = orientation[0]
-        new_msg.pose.orientation.y = orientation[1]
-        new_msg.pose.orientation.z = orientation[2]
-        new_msg.pose.orientation.w = orientation[3]
-        
-        new_msg.pose.position.x = position[0]
-        new_msg.pose.position.y = position[1]
-        new_msg.pose.position.z = position[2]
-
-        self.publisher_.publish(new_msg)
-
-class Localization(Node):
+class Grid(Node):
     def __init__(self):
-        super().__init__('overthruster') # type: ignore
-        self.publisher_ = self.create_publisher(PoseStamped, '/camera_pose', 10)
+        super().__init__('grid') # type: ignore
+        self.publisher_ = self.create_publisher(OccupancyGrid, '/light_grid', 10)
 
-    def publish(self, Rt, position):
-        msg = PoseStamped()
+    def publish(self, img, cx, cy):
+        msg = OccupancyGrid()
         msg.header.frame_id = 'world'
         msg.header.stamp = self.get_clock().now().to_msg()
 
-        orientation = Rot.from_matrix(Rt).as_quat()
-        msg.pose.orientation.x = orientation[0]
-        msg.pose.orientation.y = orientation[1]
-        msg.pose.orientation.z = orientation[2]
-        msg.pose.orientation.w = orientation[3]
-        
-        msg.pose.position.x = position[2] / 10
-        msg.pose.position.y = position[0] / -10
-        msg.pose.position.z = position[1] / 10
-
+        # Fill in metadata (MapMetaData)
+        msg.info = MapMetaData()
+        msg.info.resolution = 0.01  # Each cell is 10cm x 10cm
+        msg.info.width = img.shape[0] # Width of the grid in cells
+        msg.info.height = img.shape[1] # Height of the grid in cells
+        msg.info.origin.position.x = float(cy) * -.01
+        msg.info.origin.position.y = float(cx) * -.01
+        msg.info.origin.position.z = 0.0
+        msg.data = img.flatten('F').tolist()
         self.publisher_.publish(msg)
 
 
 rclpy.init()
-node = Localization()
+mcgriddle = Grid()
 
 detector = cv2.aruco.ArucoDetector(
     cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50),
@@ -100,7 +56,7 @@ while True:
     # flatten corners into usable format, skip if none detected
     if corners != ():
         flat_corners = ids.flatten().tolist()
-        if not (flat_corners != [0, 1] and flat_corners != [1, 0] and flat_corners != [0]):
+        if not (flat_corners != [0, 1] and flat_corners != [1, 0] and flat_corners != [0]):           
             # construct real and camera point comparison matricies
             img_points = []
             real_points = []
@@ -114,45 +70,29 @@ while True:
             # solve PnP
             _, rvec, tvec = cv2.solvePnP(real_points, img_points, mtx, dist)
 
-            # transform into world frame
-            rot = cv2.Rodrigues(rvec)[0]
-            
-            # inverse to camera localization is specified
-            if INVERSE_LOCALIZATION:
-                R = rot.transpose()
-                pos = -R * tvec
-                ZYX, jac = cv2.Rodrigues(rvec)
-                totalrotmax = np.array([[ZYX[0, 0], ZYX[0, 1], ZYX[0, 2], tvec[0][0]], [ZYX[1, 0], ZYX[1, 1], ZYX[1, 2], tvec[1][0]], [ZYX[2, 0], ZYX[2, 1], ZYX[2, 2], tvec[2][0]], [0, 0, 0, 1]])
-                rot = np.linalg.inv(totalrotmax)
-                
-            if DISPLAY:
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                font_scale = .5
-                color = (255, 255, 255)  # White color for text
-                thickness = 1
-                position = (10, frame.shape[0] - 10)
-                ypr = Rot.from_matrix(rot).as_euler('xyz', degrees=True)
-                yaw = ypr[1]
-                pitch = ypr[0]
-                roll = ypr[2]
-                x = tvec[0, 0]
-                y = tvec[1, 0]
-                z = tvec[2, 0]
-                cv2.putText(frame, f"x: {x:.2f}", (0, frame.shape[0] - 20), font, font_scale, color, thickness, cv2.LINE_AA)
-                cv2.putText(frame, f"y: {y:.2f}", (120, frame.shape[0] - 20), font, font_scale, color, thickness, cv2.LINE_AA)
-                cv2.putText(frame, f"z: {z:.2f}", (240, frame.shape[0] - 20), font, font_scale, color, thickness, cv2.LINE_AA)
-                cv2.putText(frame, f"yaw: {yaw:.2f}", (0, frame.shape[0] - 5), font, font_scale, color, thickness, cv2.LINE_AA)
-                cv2.putText(frame, f"pitch: {pitch:.2f}", (120, frame.shape[0] - 5), font, font_scale, color, thickness, cv2.LINE_AA)
-                cv2.putText(frame, f"roll: {roll:.2f}", (240, frame.shape[0] - 5), font, font_scale, color, thickness, cv2.LINE_AA)
-            
-            node.publish(rot, tvec.flatten())
+            # find homographic transform for frame
+            tag_size = 100  # desired size of the ArUco tag in the output image
+            points_dst = np.array([[0, 0], [tag_size, 0], [tag_size, tag_size], [0, tag_size]], dtype='float32')
+            h_matrix, _ = cv2.findHomography(img_points, points_dst)
+            height, width = frame.shape[:2]
+            image_corners = np.array([[0, 0], [width, 0], [width, height], [0, height]], dtype='float32')
+            transformed_corners = cv2.perspectiveTransform(np.array([image_corners]), h_matrix)[0]
+            [x_min, y_min] = np.int32(transformed_corners.min(axis=0)) # type: ignore
+            [x_max, y_max] = np.int32(transformed_corners.max(axis=0)) # type: ignore
+            output_width, output_height = x_max - x_min, y_max - y_min
+            translation_matrix = np.array([[1, 0, -x_min], [0, 1, -y_min], [0, 0, 1]])
+            adjusted_h_matrix = translation_matrix @ h_matrix
+            frame = cv2.warpPerspective(frame, adjusted_h_matrix, (output_width, output_height)) # type: ignore
 
-    cv2.imshow('Camera', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-        
+            new_corners, new_ids, _ = detector.detectMarkers(frame)
+            if new_ids is not None and 0 in new_ids[0]:
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                _, frame = cv2.threshold(frame, 60, 255, cv2.THRESH_BINARY)
+                grid = ((frame / 255) * 127).astype(np.int8)
+                mcgriddle.publish(grid, np.mean(new_corners[0][0][:, 0]), np.mean(new_corners[0][0][:, 1]))
 
-# Start pose transformation node
-# rclpy.init()
+    # cv2.imshow('Camera', frame)
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+    #     break
 
 rclpy.shutdown()
